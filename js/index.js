9901a0f1270acba134e6d7a6ec08437d546fa9e6
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-05-22 17:04:54 +0300

    Сделал 1 задачу

--- js/index.js
+++ js/index.js
@@ -3,58 +3,22 @@ import { colors } from "./task.js";
 const onClickStart = document.querySelector(`[data-action="start"]`);
 const onClickStop = document.querySelector(`[data-action="stop"]`);
 
+let isColor = null;
+
 const randomIntegerFromInterval = (min, max) => {
 return Math.floor(Math.random() * (max - min + 1) + min)};
 
-
-
-
-/*const aaa = () => {
-    return new Promise(resolve => {
-     setTimeout(() => {
-       resolve(
-     document.body.style.backgroundColor = colors[randomIntegerFromInterval(1, 6)])
-     }, 1000);
-    });
-}*/
-
-
-/*const aaa = () => {
-     setTimeout(() => {
-       document.body.style.backgroundColor = colors[randomIntegerFromInterval(1, 6)]
-       aaa()
-     }, 1000);
-}*/
-
-const onStart = () => {
-     setInterval(() => {
-       document.body.style.backgroundColor = colors[randomIntegerFromInterval(1, 6)]
-     }, 1000);
+const numeral = () => {
+document.body.style.backgroundColor = colors[randomIntegerFromInterval(1, 6)]
 }
 
-
-onClickStart.addEventListener("click", onStart);
-onClickStop.addEventListener("click", () => {
-  clearInterval(onStart)
+onClickStart.addEventListener("click", () => {
+  isColor = setInterval(() => {
+    numeral();
+  }, 1000);
 });
 
+onClickStop.addEventListener("click", () => {
+  clearInterval(isColor);
+});
 
-
-
-
-
-/*onClickStart.addEventListener("click", () => {
- new Promise(resolve => {
-    setTimeout(() => {
-      resolve(aaa)
-    }, 1000);
-  });
-});*/
-
-/*onClickStart.addEventListener("click", () => {
-  aaa().then(data => (data));
-});*/
-
-
-
-//onClickStop.addEventListener("click",);
